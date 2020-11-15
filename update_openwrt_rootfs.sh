70f136024aebf300f770aba996fbb9a9ef8db3b6
Kayuii <577738@qq.com>
2020-11-15 16:21:04 +0800

    fix --platform can't be used

--- update_openwrt_rootfs.sh
+++ update_openwrt_rootfs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -xe
-curl -sL https://downloads.openwrt.org/snapshots/targets/x86/generic/openwrt-x86-generic-rootfs.tar.gz | docker import --platform arm64 - kayuii/openwrt-rootfs:latest
+curl -sL https://downloads.openwrt.org/snapshots/targets/x86/generic/openwrt-x86-generic-rootfs.tar.gz | docker import - kayuii/openwrt-rootfs:latest
 docker push kayuii/openwrt-rootfs:latest
 
-curl -sL http://downloads.openwrt.org/releases/19.07.4/targets/x86/generic/openwrt-19.07.3-x86-generic-generic-rootfs.tar.gz | docker import --platform arm64 - kayuii/openwrt-rootfs:19.07.4
+curl -sL http://downloads.openwrt.org/releases/19.07.4/targets/x86/generic/openwrt-19.07.4-x86-generic-generic-rootfs.tar.gz | docker import - kayuii/openwrt-rootfs:19.07.4
 docker push kayuii/openwrt-rootfs:19.07.4
 
-curl -sL https://downloads.openwrt.org/snapshots/targets/armvirt/64/openwrt-armvirt-64-rootfs-squashfs.img.gz | docker import --platform armvirt64 - kayuii/openwrt-rootfs:latest
-docker push kayuii/openwrt-rootfs:latest
+curl -sL https://downloads.openwrt.org/snapshots/targets/armvirt/64/openwrt-armvirt-64-default-rootfs.tar.gz | docker import - kayuii/openwrt-rootfs:armvirt64
+docker push kayuii/openwrt-rootfs:armvirt64
 
-curl -sL https://downloads.openwrt.org/releases/19.07.4/targets/armvirt/64/openwrt-19.07.4-armvirt-64-root.squashfs.gz | docker import --platform armvirt64 - kayuii/openwrt-rootfs:19.07.4
-docker push kayuii/openwrt-rootfs:19.07.4
+curl -sL https://downloads.openwrt.org/releases/19.07.4/targets/armvirt/64/openwrt-19.07.4-armvirt-64-default-rootfs.tar.gz | docker import - kayuii/openwrt-rootfs:armvirt64-19.07.4
+docker push kayuii/openwrt-rootfs:armvirt64-19.07.4
