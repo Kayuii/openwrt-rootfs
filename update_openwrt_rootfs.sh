3f0dd4e918cf031ac9069d7fa8105cd0ae077486
Kayuii <577738@qq.com>
2020-11-15 16:32:32 +0800

    fix --platform can't be used

--- update_openwrt_rootfs.sh
+++ update_openwrt_rootfs.sh
@@ -6,8 +6,8 @@ docker push kayuii/openwrt-rootfs:latest
 curl -sL http://downloads.openwrt.org/releases/19.07.4/targets/x86/generic/openwrt-19.07.4-x86-generic-generic-rootfs.tar.gz | docker import - kayuii/openwrt-rootfs:19.07.4
 docker push kayuii/openwrt-rootfs:19.07.4
 
-curl -sL https://downloads.openwrt.org/snapshots/targets/armvirt/64/openwrt-armvirt-64-default-rootfs.tar.gz | docker import - kayuii/openwrt-rootfs:armvirt64
+curl -sL https://downloads.openwrt.org/snapshots/targets/armvirt/64/openwrt-armvirt-64-default-rootfs.tar.gz | docker import --platform=armvirt64 - kayuii/openwrt-rootfs:armvirt64
 docker push kayuii/openwrt-rootfs:armvirt64
 
-curl -sL https://downloads.openwrt.org/releases/19.07.4/targets/armvirt/64/openwrt-19.07.4-armvirt-64-default-rootfs.tar.gz | docker import - kayuii/openwrt-rootfs:armvirt64-19.07.4
+curl -sL https://downloads.openwrt.org/releases/19.07.4/targets/armvirt/64/openwrt-19.07.4-armvirt-64-default-rootfs.tar.gz | docker import --platform=aacrh64 - kayuii/openwrt-rootfs:armvirt64-19.07.4
 docker push kayuii/openwrt-rootfs:armvirt64-19.07.4
